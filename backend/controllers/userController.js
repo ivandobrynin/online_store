5ccf1706267b1cc284163a60d8249dd230824efd
DESKTOP-H9TS9F3\drugg <druggruzovik@gmail.com>
2021-09-07 19:47:38 +0300

    server added

---
+++ backend/controllers/userController.js
@@ -0,0 +1,59 @@
+const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const { User, Basket, BasketDevice } = require('../models/models')
+
+const generateToken = (id, email, role) => {
+	return token = jwt.sign(
+		{id, email, role}, 
+		process.env.SECRET_KEY,
+		{expiresIn: '24h'}
+	)
+}
+
+class UserController {
+	async registration (req, res, next) {
+		const { email, password, role } = req.body
+		if (!email && !password) {
+			return next(ApiError.badRequest('Некорректные данные'))
+		}
+		const candidate = await User.findOne({where: {email}})
+		if (candidate) {
+			return next(ApiError.badRequest('Пользователь уже существует'))
+		}
+
+		const hashPassword = await bcrypt.hash(password, 5)
+
+		const user = await User.create({email, role, password: hashPassword})
+		const basket = await Basket.create({userId: user.id})
+
+		const token = generateToken(user.id, user.email, user.role)
+		return res.json({token})
+	}
+
+	async login (req, res, next) {
+		const { email, password } = req.body
+		const user = await User.findOne({where: {email}})
+
+		if (!user) {
+			return next(ApiError.badRequest(`Пользователя ${email} не существует`))
+		}
+
+		let comparePassword = bcrypt.compareSync(password, user.password)
+
+		if (!comparePassword) {
+			return next(ApiError.badRequest(`Неверный пароль`))
+		}
+
+		const token = generateToken(user.id, user.email, user.role)
+		return res.json({token})
+	}
+
+	async checkAuth (req, res, next) {
+		const { user } = req.body
+		const token = generateToken(user.id, user.email, user.role)
+		return res.json({token})
+	}
+}
+
+module.exports = new UserController()
---
+++ backend/routes/index.js
@@ -0,0 +1,13 @@
+const Router = require('express')
+const router = new Router()
+const userRouter = require('./userRouter')
+const deviceRouter = require('./deviceRouter')
+const typeRouter = require('./typeRouter')
+const brandRouter = require('./brandRouter')
+
+router.use('/user', userRouter)
+router.use('/device', deviceRouter)
+router.use('/type', typeRouter)
+router.use('/brand', brandRouter)
+
+module.exports = router
